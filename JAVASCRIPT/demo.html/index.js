d6b59c7f45e62014c844e3dab94e97490066def2
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-01-18 22:17:19 +0530

    last

--- JAVASCRIPT/demo.html/index.js
+++ JAVASCRIPT/demo.html/index.js
@@ -9,10 +9,10 @@ body.append(h1, mob)
 
 
 const calculate = () =>{
-   const a=12;
+   const a=1546;
    const b=40;
    const c = a+b;
-   return c;
+   console.log(c)
 }
 
 calculate();
@@ -24,3 +24,25 @@ function myf(){
    return c;
 }
 console.log(myf())
+
+const myFun = () => {
+   const obj = {
+      name: "omkar",
+      age: 26,
+      mob: 124578787,
+      hobbies:["running", "biking"],
+      add: {
+         city:"jaunpur",
+         country:"India",
+      }
+   }
+  
+   obj.add["pin"]="222";
+   console.log(obj.add,)
+   for(let i=0; i<obj.hobbies.length; i++){
+      console.log(obj.hobbies[i])
+   }
+   console.log(obj)
+}
+
+myFun();
