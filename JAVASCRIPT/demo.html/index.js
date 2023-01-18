52f9d4745f2fdb0d7540b4209d4c79c5e9e4dbb4
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-01-18 21:56:13 +0530

    mn

--- JAVASCRIPT/demo.html/index.js
+++ JAVASCRIPT/demo.html/index.js
@@ -6,3 +6,21 @@ const h1 = document.createElement("h1");
 
    mob.innerHTML = "202211002"
 body.append(h1, mob)
+
+
+const calculate = () =>{
+   const a=12;
+   const b=40;
+   const c = a+b;
+   return c;
+}
+
+calculate();
+
+function myf(){
+   const a=12;
+   const b=40;
+   const c = a+b;
+   return c;
+}
+console.log(myf())
