e8edb7c0b3c49a6fdbe92dbbe40068b89c890a8a
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-01-18 21:46:06 +0530

    new

--- JAVASCRIPT/demo.html/index.js
+++ JAVASCRIPT/demo.html/index.js
@@ -1,6 +1,8 @@
-const body = document.getElementsByTagName("body");
+const body = document.querySelector("body");
 
 const h1 = document.createElement("h1");
    h1.innerText = "Omkar"
+   const mob = document.createElement("p");
 
-body.append(h1)
+   mob.innerHTML = "202211002"
+body.append(h1, mob)
