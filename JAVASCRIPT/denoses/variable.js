9cdbad5564b7c3c316be853ab695c2728bdde369
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-01-21 17:31:59 +0530

    agan

--- JAVASCRIPT/denoses/variable.js
+++ JAVASCRIPT/denoses/variable.js
@@ -1,132 +1,153 @@
-const body = document.querySelector("body");
+// const body = document.querySelector("body");
 
-const h1 = document.createElement("h1");
-   h1.innerText = "Omkar"
-   const mob = document.createElement("p");
+// const h1 = document.createElement("h1");
+//    h1.innerText = "Omkar"
+//    const mob = document.createElement("p");
 
-   mob.innerHTML = "202211002"
+//    mob.innerHTML = "202211002"
 
-    const btn = document.createElement("button")
+//     const btn = document.createElement("button")
 
-          btn.innerText = "submit"
+//           btn.innerText = "submit"
 
-          btn.addEventListener("click", myClick);
+//           btn.addEventListener("click", myClick);
             
-          function myClick(){
-            const name = document.createElement("h2");
-            if (name.innerText === "") {
-               name.innerText = "omkar chauhan";
-           } else {
-               name.innerText = "";
-               alert("clicked again")
-           }
-                body.append(name)
-         }
+//           function myClick(){
+//             const name = document.createElement("h2");
+//             if (name.innerText === "") {
+//                name.innerText = "omkar chauhan";
+//            } else {
+//                name.innerText = "";
+//                alert("clicked again")
+//            }
+//                 body.append(name)
+//          }
          
      
       
 
-body.append(h1, mob, btn)
+// body.append(h1, mob, btn)
 
 
 
-const calculate = () => {
-   const a=16;
-   const b=40;
-   const c = a+b;
-   console.log(c)
-}
+// const calculate = () => {
+//    const a=16;
+//    const b=40;
+//    const c = a+b;
+//    console.log(c)
+// }
 
-calculate();
+// calculate();
 
-function myf(){
-   const a=12;
-   const b=40;
-   const c = a+b;
-   return c;
-}
-console.log(myf())
+// function myf(){
+//    const a=12;
+//    const b=40;
+//    const c = a+b;
+//    return c;
+// }
+// console.log(myf())
 
-const myFun = () => {
-   const obj = {
-      name: "omkar",
-      age: 26,
-      mob: 124578787,
-      hobbies:["running", "biking"],
-      add: {
-         city:"jaunpur",
-         country:"India",
-      }
-   }
+// const myFun = () => {
+//    const obj = {
+//       name: "omkar",
+//       age: 26,
+//       mob: 124578787,
+//       hobbies:["running", "biking"],
+//       add: {
+//          city:"jaunpur",
+//          country:"India",
+//       }
+//    }
   
-   obj.add["pin"]="222";
-   console.log(obj.add,)
-   for(let i=0; i<obj.hobbies.length; i++){
-      console.log(obj.hobbies[i])
-   }
-   console.log(obj)
-}
+//    obj.add["pin"]="222";
+//    console.log(obj.add,)
+//    for(let i=0; i<obj.hobbies.length; i++){
+//       console.log(obj.hobbies[i])
+//    }
+//    console.log(obj)
+// }
 
-myFun();
+// myFun();
 
-let name = "omkar";
-let bag =  "";
+// let name = "omkar";
+// let bag =  "";
 
-for(let i=0; i<name.length; i++){
-   if(i%2==0){
-      bag+=name[i].toUpperCase();
-   }else{
-      bag+=name[i].toLowerCase();
-   }
-}
-console.log(bag)
-
-
-let fname = "Omkar";
-
-let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let lower = "abcdefghijklmnopqrstuvwxyz";
+// for(let i=0; i<name.length; i++){
+//    if(i%2==0){
+//       bag+=name[i].toUpperCase();
+//    }else{
+//       bag+=name[i].toLowerCase();
+//    }
+// }
+// console.log(bag)
 
 
-let jhola = "";
+// let fname = "Omkar";
 
-for(let i=0; i<fname.length; i++){
+// let upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let lower = "abcdefghijklmnopqrstuvwxyz";
+
+
+// let jhola = "";
+
+// for(let i=0; i<fname.length; i++){
   
-   for(let j=0; j<upper.length; j++){
-      if(fname[i]==upper[j]){
-         jhola+=lower[j];
-      }
+//    for(let j=0; j<upper.length; j++){
+//       if(fname[i]==upper[j]){
+//          jhola+=lower[j];
+//       }
 
-   }
+//    }
   
+// }
+// console.log(jhola)
+
+
+
+
+// // ASSIGNMENT
+
+// let myName = "omkar";
+//   console.log(typeof(myName))
+
+
+// let nameIsstring = true;
+
+// console.log(typeof(nameIsstring))
+
+// let lastName;
+
+// console.log(typeof(lastName))
+
+// let mobi
+// console.log(typeof(mobi))
+
+
+
+
+    
+let body = document.querySelector("body")
+
+body.addEventListener("click", myClick);
+
+function myClick(){
+
+   let div = document.createElement("div");
+
+
+   div.classList.add("circle")
+
+   div.style.left = Eventxp
+
+   body.appendChild(div)
 }
-console.log(jhola)
+
+  let val = confirm("Are you sure?")
+  if(val){
+   document.
+  }
 
 
-
-
-// ASSIGNMENT
-
-let myName = "omkar";
-  console.log(typeof(myName))
-
-
-let nameIsstring = true;
-
-console.log(typeof(nameIsstring))
-
-let lastName;
-
-console.log(typeof(lastName))
-
-let mobi
-console.log(typeof(mobi))
-
-
-
-
-
-
-
+  let prop = prompt("solve this 2-1=?")
 
 
