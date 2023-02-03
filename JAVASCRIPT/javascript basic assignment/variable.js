3c7b06cec69b80d0b686ade4077e6752cf816bfb
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-03 14:15:11 +0530

    alst

--- JAVASCRIPT/javascript basic assignment/variable.js
+++ JAVASCRIPT/javascript basic assignment/variable.js
@@ -773,3 +773,31 @@ function getFileExtension(filename) {
 
 let filename = "document.txt";
 console.log("The extension of the file is: " + getFileExtension(filename));
+
+
+
+
+// 30. ANS ==> 
+
+function myCalculator(num1, num2, operator) {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    case "/":
+      return num1 / num2;
+    default:
+      return "Invalid operator";
+  }
+}
+
+let num1 = 10;
+let num2 = 5;
+
+console.log("Addition: " + myCalculator(num1, num2, "+"));
+console.log("Substraction: " + myCalculator(num1, num2, "-"));
+console.log("Multiplication: " + myCalculator(num1, num2, "*"));
+console.log("Division: " + myCalculator(num1, num2, "/"));
