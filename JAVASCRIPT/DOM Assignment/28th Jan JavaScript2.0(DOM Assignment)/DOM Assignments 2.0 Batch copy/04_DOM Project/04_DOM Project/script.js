bc993a4f74dedd44406c0609b4a00eb46fc8a40f
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-07 12:51:05 +0530

    second commit

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
@@ -1,7 +1,14 @@
 
+// title menthioned using DOM
+const second_title = document.querySelector(".title_2")
+second_title.innerHTML = "The Archer"
+const fourth_title = document.querySelector(".title_4")
+fourth_title.innerHTML = "The Goblin"
 
-const wizard = document.getElementsByClassName("clash-card__unit-stats");
+
+// background Change using DOM
+const wizard = document.getElementsByClassName("");
 wizard.style.backgroundColor = "red"
 
-const body = document.querySelector("body");
+
 
