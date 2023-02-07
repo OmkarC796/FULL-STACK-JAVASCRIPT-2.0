f773a03e8fe7927f625d0137487cb4bccfc7f75a
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-07 13:07:12 +0530

    third commit is

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
@@ -1,14 +1,34 @@
 
 // title menthioned using DOM
+
 const second_title = document.querySelector(".title_2")
 second_title.innerHTML = "The Archer"
+
 const fourth_title = document.querySelector(".title_4")
 fourth_title.innerHTML = "The Goblin"
 
 
 // background Change using DOM
-const wizard = document.getElementsByClassName("");
-wizard.style.backgroundColor = "red"
+
+const Barbarian_bottom_div = document.querySelector("#first_bottom");
+Barbarian_bottom_div.style.backgroundColor = "#ec9b3b"
+Archer_bottom_div.style.color = "#ffffff"
+
+const Archer_bottom_div = document.querySelector("#second_bottom");
+Archer_bottom_div.style.backgroundColor = "#ee5487"
+Archer_bottom_div.style.color = "#ffffff"
+
+const Giant_bottom_div = document.querySelector("#third_bottom");
+Giant_bottom_div.style.backgroundColor = "#f6901a"
+Giant_bottom_div.style.color = "#ffffff"
+
+const Goblin_bottom_div = document.querySelector("#fourth_bottom");
+Goblin_bottom_div.style.backgroundColor = "#82bb30"
+Goblin_bottom_div.style.color = "#ffffff"
+
+const Wizard_bottom_div = document.querySelector("#last_bottom");
+Wizard_bottom_div.style.backgroundColor = "#4facff"
+Wizard_bottom_div.style.color = "#ffffff"
 
 
 
