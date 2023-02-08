ea9b68d1c22cbd67faab76c4f58fea2cb578c7d3
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-08 10:02:44 +0530

    initial commit

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/04_DOM Project/04_DOM Project/script.js
@@ -1,14 +1,14 @@
 let title = document.querySelector("title")
-title.innerText = "04_DOM_Project"
+title.innerText = "04_DOM_Project";
 
 
 // title menthioned using DOM
 
 const second_title = document.querySelector(".title_2")
-second_title.innerHTML = "The Archer"
+second_title.innerHTML = "The Archer";
 
 const fourth_title = document.querySelector(".title_4")
-fourth_title.innerHTML = "The Goblin"
+fourth_title.innerHTML = "The Goblin";
 
 
 // background Change using DOM
@@ -22,7 +22,7 @@ Archer_bottom_div.style.backgroundColor = "#ee5487";
 Archer_bottom_div.style.color = "#ffffff";
 
 const Giant_bottom_div = document.querySelector("#third_bottom");
-Giant_bottom_div.style.backgroundColor = "#f6901a"
+Giant_bottom_div.style.backgroundColor = "#f6901a";
 Giant_bottom_div.style.color = "#ffffff";
 
 const Goblin_bottom_div = document.querySelector("#fourth_bottom");
@@ -32,6 +32,11 @@ Goblin_bottom_div.style.color = "#ffffff";
 const Wizard_bottom_div = document.querySelector("#last_bottom");
 Wizard_bottom_div.style.backgroundColor = "#4facff";
 Wizard_bottom_div.style.color = "#ffffff";
+
+// const title_name = document.querySelector("#title_1");
+// title_name.style.color = "#000000";
+// const title_name = document.querySelector("#title_2");
+// title_name.style.color = "#000000";
 
 
 
