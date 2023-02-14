c7df711c6f4242bd25f7fc72577974f23e8742a4
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-14 13:51:11 +0530

    new dom task

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
@@ -1,3 +1,37 @@
+
+// task1 add new project menu to navber 
+const chenging_project = document.querySelector(".chenging_project");
+
+chenging_project.innerHTML = "Projects";
+
+
+// task2 add new accordian div with new strength and description
+
+const accordian_wrapper = document.querySelector(".accordian-wrapper");
+
+const accordian_div = document.createElement("div");
+accordian_div.setAttribute("class", "accordian");
+
+const h3 = document.createElement("h3");
+h3.innerHTML = "Skills";
+
+const p = document.createElement("p");
+
+
+p.innerHTML = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the GitHub."
+
+accordian_div.append(h3, p);
+
+accordian_wrapper.append(accordian_div)
+
+
+
+
+
+
+
+
+
 let accordian = document.querySelectorAll(".accordian h3");
 accordian.forEach((element) => {
   element.addEventListener("click", () => {
@@ -11,24 +45,4 @@ accordian.forEach((element) => {
 });
 
 
-const chenging_project = document.querySelector(".chenging_project");
-
-chenging_project.innerHTML = "Projects";
-
-
-
-const accordian_wrapper = document.querySelector(".accordian-wrapper");
-
-const accordian_div = document.createElement("div");
-accordian_div.classList.add = "accordian";
-
-const h3 = document.createElement("h3");
-h3.innerHTML = "Skills";
-
-const p = document.createElement("p");
-p.innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the GitHub."
-
-accordian_div.append(h3, p);
-
-accordian_wrapper.append(accordian_div)
 
