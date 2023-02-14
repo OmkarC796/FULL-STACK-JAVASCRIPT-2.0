cc331724c0419bce0aa34eaf3c6013249b0d127a
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-14 13:37:50 +0530

    new initial commit

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM Assignment 2.0 1,2,3/about/about.js
@@ -9,3 +9,26 @@ accordian.forEach((element) => {
     }
   });
 });
+
+
+const chenging_project = document.querySelector(".chenging_project");
+
+chenging_project.innerHTML = "Projects";
+
+
+
+const accordian_wrapper = document.querySelector(".accordian-wrapper");
+
+const accordian_div = document.createElement("div");
+accordian_div.classList.add = "accordian";
+
+const h3 = document.createElement("h3");
+h3.innerHTML = "Skills";
+
+const p = document.createElement("p");
+p.innerText = "I posses a very good command over the Full Stack Development technologies like MERN which can be seen in my work over the GitHub."
+
+accordian_div.append(h3, p);
+
+accordian_wrapper.append(accordian_div)
+
