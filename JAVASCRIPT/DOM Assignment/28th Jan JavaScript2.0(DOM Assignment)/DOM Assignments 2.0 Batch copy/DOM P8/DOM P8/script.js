ae659e27de9a74c6c6ebde8926fe52da2b786d0f
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-16 10:34:33 +0530

    new commit initial

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/script.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P8/DOM P8/script.js
@@ -1,13 +1,27 @@
+"use strict"
 
-
+// task: 1 => added new hr h2 and p elements.
 const new_div = document.querySelector(".new");
 
-// new_div.style.overFlow = "hidden"
+new_div.style.overflow = "scroll"
 
 const new_hr = document.createElement("hr");
-new_hr.setAttribute("class", "hr_line")
+new_hr.setAttribute("class", "hr-line")
 
 const h2 = document.createElement("h2");
-h2.setAttribute("class", "new_head")
-h2.innerText = "omkar"
-new_div.append(new_hr, h2)
+h2.setAttribute("class", "new-head")
+
+
+h2.innerHTML = "This is my custom heading";
+
+
+const p = document.createElement("p");
+p.setAttribute("class", "new-p")
+p.innerText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, ipsam cumque totam facilis magnam natus vel dignissimos ipsum recusandae  fuga."
+
+new_div.append(new_hr, h2, p);
+
+
+// task: 2 => normal back
+const body = document.querySelector("body");
+      body.style.backgroundImage = "none"
