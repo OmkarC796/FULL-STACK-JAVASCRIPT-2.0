de4b72984a079d1aeb5bb132f45b52aa524f38ae
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-19 23:35:09 +0530

    this is for each arrow f

--- JAVASCRIPT/array.js
+++ JAVASCRIPT/array.js
@@ -717,3 +717,9 @@ const arrT = [1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,20]
 arrT.map((e) => {
   console.log(edge(edge(e)))
 })
+
+const ech = (e) => {
+  arrT.forEach((e) => {
+    console.log(e)
+  })
+}
