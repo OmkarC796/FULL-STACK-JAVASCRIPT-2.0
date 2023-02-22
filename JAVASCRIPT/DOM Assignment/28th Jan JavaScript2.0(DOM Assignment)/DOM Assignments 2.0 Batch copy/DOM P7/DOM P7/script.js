d2a23b5681d06aa13ae8b019a0c20675305a835f
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-02-22 18:25:23 +0530

    nwj

--- JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/script.js
+++ JAVASCRIPT/DOM Assignment/28th Jan JavaScript2.0(DOM Assignment)/DOM Assignments 2.0 Batch copy/DOM P7/DOM P7/script.js
@@ -19,26 +19,17 @@ for(let i=0; i<removed_languages.length; i++){
 // task: 2
 
 
+     
+function catchInput() {
+  let value = document.querySelector("input").value;
+  localStorage.setItem("input", JSON.stringify(value));
+}
+
 let input = document.querySelector("input");
-    input.disabled = false
+input.disabled = false;
 
 const submit_button = document.querySelector(".main__form-btn");
-      submit_button.disabled = false;
-      submit_button.style.cursor = "pointer"
-      
-
-      submit_button.addEventListener("submit",myfn)
-      
-      
-      
-     function myfn(){
-        const input = document.querySelector("input").value
-        document.localStorage.setItem("inputValue", JSON.stringify(input))
-      }
-
-console.log(myfunc())
-
- 
-
-
-
+submit_button.addEventListener("click", catchInput);
+submit_button.style.cursor = "pointer";
+    submit_button.disabled = false;
+  
