bd63ea7e88a204b178437a2f0a54fc6e8babf6d2
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-02 22:06:10 +0530

    new

--- JAVASCRIPT/JS-Practice/fetchAPI.js
+++ JAVASCRIPT/JS-Practice/fetchAPI.js
@@ -6,6 +6,6 @@ const url = "https://restcountries.com/v2/all"; // country name and some data fe
 
 fetch(url)
 .then((response) => response.json())
-.then((data) => console.log(data))
+.then((data) => console.log(data))            // this is the fetch request .then and .catch methods
 .catch((error) => console.log(error))
 
