fa2469ec75f96ac0608598b249aa7db054f08979
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-13 00:41:57 +0530

    new initial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
+function appTwo(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>App Two</h1>
     </div>
   );
 }
+
+function App() {
+  return (
+    
+   <div>
+      <h1>App</h1>
+      <p>Hello this is very first react on my vs code editor</p>
+      <appTwo/>
+   </div>
+   
+  );
+}
+
 
 export default App;
