d8d30be7d716bba7ef9c16157fcf49c3f7d9c4c9
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-13 00:54:01 +0530

    jsx code basics

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-function appTwo(){
+function Apptwo() {
   return (
     <div>
       <h1>App Two</h1>
@@ -15,7 +15,7 @@ function App() {
    <div>
       <h1>App</h1>
       <p>Hello this is very first react on my vs code editor</p>
-      <appTwo/>
+      <Apptwo/>
    </div>
    
   );
