6e16653e0830a9d3f4950cd7d72b0cda6b305830
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-13 09:06:33 +0530

    counter react app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,46 @@
 // import logo from './logo.svg';
 // import './App.css';
 
-function Apptwo() {
-  return (
-    <div>
-      <h1>App Two</h1>
-    </div>
-  );
+import {useState} from 'react';
+
+// function Apptwo() {
+//   return (
+//     <div>
+//       <h1>App Two</h1>
+//     </div>
+//   );
+// }
+
+
+
+function Counter() {
+  let [count, setCount] = useState(100)
+
+ function oneUp(){
+  setCount(count + 1);
+ }
+
+ return (
+  <div>
+     <button
+     onClick={oneUp}>
+        Count : {count}
+  </button>
+  </div>
+ );
 }
+
+
 
 function App() {
   return (
     
-   <div>
+   <>
       <h1>App</h1>
       <p>Hello this is very first react on my vs code editor</p>
-      <Apptwo/>
-   </div>
+    
+      <Counter/>
+   </>
    
   );
 }
