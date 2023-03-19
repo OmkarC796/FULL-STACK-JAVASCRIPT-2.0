d6bc0d9d37ebe74c517a4b97c17d06d345f72515
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-19 11:42:00 +0530

    new commit

--- React.JS/second-react-lectr/src/App.js
+++ React.JS/second-react-lectr/src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Card from "./card";
 
 function App() {
     return(
+        <>
         <div>App</div>
+        </>
     )
     
 }
---
+++ React.JS/second-react-lectr/src/card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function card() {
+  return (
+    <div>This is a card</div>
+  )
+}
+
+export default card
