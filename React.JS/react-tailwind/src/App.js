9ed0e0123b0d2d66fc88434c48dc83e41f4eb7a4
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-21 21:30:27 +0530

    new initial

--- React.JS/react-tailwind/src/App.js
+++ React.JS/react-tailwind/src/App.js
@@ -3,7 +3,7 @@
 function App() {
   return (
     <>
-    <h1 className="text-red-800 font-extrabold">Hello React</h1>
+    <h1 className="text-red-800 font-extrabold bg-gray-900">Hello React</h1>
     </>
   );
 }
