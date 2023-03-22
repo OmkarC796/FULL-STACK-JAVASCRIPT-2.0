454a366f2457a55fef5b1b36845ed43b06538858
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-22 10:12:08 +0530

    new

--- React.JS/react-tailwind/src/Components/Navbar.js
+++ React.JS/react-tailwind/src/Components/Navbar.js
@@ -1,9 +1,11 @@
+import Cards from "./Cards";
 
 
 function App() {
     return (
       <>
       <h1 className="text-red-800 font-extrabold bg-gray-900">Hello React</h1>
+      <Cards/>
       </>
     );
   }
