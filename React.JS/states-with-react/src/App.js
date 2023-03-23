e4d43b77d90733ab0a51dc88e1b620d2bbbd899f
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-23 19:19:47 +0530

    new final app

--- React.JS/states-with-react/src/App.js
+++ React.JS/states-with-react/src/App.js
@@ -3,13 +3,22 @@ import React, {useState} from 'react';
 function App() {
 
   const [count, setCount] = useState(0)
+
+const incrementCount = () => {
+  setCount(count+1)
+}
+
+const decrementCount = () => {
+  setCount(count-1)
+}
+
   return (
     <div className="counter">
       <h1>App counter</h1>
       <div> {count} </div>
       <div className="buttons">
-        <button></button>
-        <button></button>
+        <button onClick={incrementCount}>+</button>
+        <button onClick={decrementCount}>-</button>
       </div>
     </div>
   );
