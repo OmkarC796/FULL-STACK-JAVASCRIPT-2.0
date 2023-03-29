39184f15e6807c633823e3c9edef7f6506f12574
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-29 13:08:30 +0530

    new

--- React.JS/react-context-api/src/App.js
+++ React.JS/react-context-api/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
      <h1>Hello React</h1>
-     <p>This app for api context</p>
+     <p>This app for api context by which can fetch the data</p>
     </>
   );
 }
