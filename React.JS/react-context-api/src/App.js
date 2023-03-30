c8176ae10ef6cf2d6a3717bacbfbec75a996ffaf
OMKAR CHAUHAN <103635381+OmkarChauhan796@users.noreply.github.com>
2023-03-30 18:34:09 +0530

    new commit

--- React.JS/react-context-api/src/App.js
+++ React.JS/react-context-api/src/App.js
@@ -1,11 +1,30 @@
+import { useState, createContext } from "react";
+import {  } from "module";
+
+import  Navbar from "./components/Navbar";
 
 import './App.css';
 
 function App() {
+const [name, setname] = useState("");
+const [color, setColor] = useState("");
+
   return (
     <>
      <h1>Hello React</h1>
      <p>This app for api context by which can fetch the data</p>
+     <Navbar/>
+     <form action="">
+      <input type={"text"}
+      placeholder="Enter User Name"
+      value={name}
+      onChange={myname  } />
+      <input type={"text"}
+      placeholder="Enter color name"
+      value={color} 
+      onChange={myColor}/>
+     </form>
+
     </>
   );
 }
